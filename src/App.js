ab1942970b30bbb6eebacaf5a36841be023b7fc1
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-03-07 21:50:32 +0100

    initial files -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react/cjs/react.production.min';
+import { useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -11,8 +11,6 @@ function App() {
         <button onClick={() => setCounter((prevCount) => prevCount - 1)} >-</button>
         <h1>{counter}</h1>
         <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-
-
       </header>
     </div>
   );
