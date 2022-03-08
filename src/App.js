0d46fc5fbe18c5df5fe82d306071854e3337a3de
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-03-09 00:58:55 +0100

    todos 32

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useState, useEffect } from 'react';
-import SearchIcon from "./search.svg";
-import MovieCard from './Movieard';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import Todo from './components/Todo';
 
-//bb2f855
-
-const API_URL = "http://www.omdbapi.com?apikey=bb2f855";
-const movie1 = {
-  Title: 'Italian Spiderman',
-  Year: '2007',
-  imdbID: 'tt2705436',
-  Type: 'movie',
-  Poster: 'https://m.media-amazon.com/images/M/MV5BYjFhN2RjZT…TkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg'
-}
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-
-    setMovies(data.Search);
-  }
-  useEffect(() => {
-    searchMovies()
-  }, [])
-
+  
   return(
     <div>
-      <h1>MovieLand</h1>
-      <div className='search' >
-        <input 
-          placeholder='search for movies' 
-          value={searchTerm}
-          onChange={(e) => {setSearchTerm(e.target.value)}}
-        />
-        <img src={SearchIcon}
-            alt="search"
-            onClick={() => {searchMovies(searchTerm)}}
-        />
-      </div>      
-        {
-          movies?.length > 0 ? (
-              <div className='container'>
-                  {movies.map((movie) => (<MovieCard movie={movie} />) )}
-              </div>
-          ) : (
-            <div className='empty' >
-              <h2>no movies found</h2>
-            </div>
-          )
-        }
+      <TodoList  /> 
     </div>
 
   )
