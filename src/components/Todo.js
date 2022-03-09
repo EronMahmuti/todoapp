10bb82c2743811a6e1c4c690481a60cfc3cf31bc
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-03-10 00:54:17 +0100

    Brian design finished, add glassmorphism

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,11 +3,22 @@ import TodoForm from './TodoForm'
 import {RiCloseCircleLine} from "react-icons/ri"
 import {TiEdit} from "react-icons/ti"
 
-function Todo({todos,completeTodo}) {
+function Todo({todos,completeTodo, removeTodo, updateTodo }) {
     const [edit, setEdit] = useState({
         id:null,
         value: ''
     });
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id:null,
+            value:''
+        })
+    }
+    if(edit.id){
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />
+    }
 
     return todos.map((todo,index) => (
         <div className={todo.isComplete ? 'todo-row complete' : ' todo-row'} key={index} >
@@ -15,8 +26,15 @@ function Todo({todos,completeTodo}) {
                 {todo.text}    
             </div>
             <div className='icons'>
-                <RiCloseCircleLine />
-                <TiEdit />
+                <RiCloseCircleLine 
+                    onClick={() => removeTodo(todo.id)}
+                    className="delete-icon"
+                />
+                <TiEdit 
+                    onClick={() => setEdit({id:todo.id, value:todo.text})}
+                    className="edit-icon"
+                    />
+
             </div>
         </div>  
     ))
